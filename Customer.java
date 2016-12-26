7bd47cc70c3c9d4e20dc83b8d30081e461496dee
umernadeem <umernadeem23@gmail.com>
2016-12-25 19:35:57 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -4,25 +4,61 @@ import java.io.*;
 // Abstract class customer
 abstract class Customer{
   protected String name; // customer name
-  protected int customerNum; // customer id
+  protected int id; // customer id
   protected int curAmountOfItems; // current amount of items checked out
   protected int itemsLate; // amount of items overdue
   
   // Constructor
   public Customer(String givenName, givenNumber, givenItems, givenItemsLate){
     name = givenName;
-    customerNum = givenNumber;
+    id = givenNumber;
     curAmountOfItems = givenItems;
     itemsLate = givenItemsLate;
   }
   
   // Returns Customer info
   public String toString(){
-    String s = "Name: " + name + "\nCustomer Number: " + customerNum + 
+    String s = "Name: " + name + "\nCustomer Number: " + id + 
       "\nAmount of items checked out " + curAmountOfItems + "\nItems Late " +
       itemsLate;
     return s;
   }
   
+  public int compareToCustomerName (Customer other){
+    if (name.compareTo(other.name) > 0)
+      return 1;
+    else if (name.compareTo(other.name) < 0)
+      return -1;
+    else
+      return 0;
+  }
+  public int compareToId (Customer other){
+    if (this.id > other.id){
+      return 1;
+    }
+    else if (other.id > this.id){
+      return -1;
+    }
+    return 0;
+  }
+  public int compareToItemsLate (Customer other){
+    if (this.itemsLate > other.itemsLate){
+      return 1;
+    }
+    else if (other.itemsLate > this.itemsLate){
+      return -1;
+    }
+    return 0;
+  }
+  
+  public int compareToCurItems (Customer other){
+    if (this.curAmountOfItmes > other.curAmountOfItems){
+      return 1;
+    }
+    else if (other.curAmountOfItems > this.curAmountOfItems){
+      return -1;
+    }
+    return 0;
+  }
   
 }
