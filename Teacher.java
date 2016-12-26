6264cb86dcdc7a3b97d09c33831af4c5cb912e17
umernadeem <umernadeem23@gmail.com>
2016-12-25 19:17:03 -0500

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -6,5 +6,16 @@ class Teacher extends Customer{
     super (name, customerNum, curItems, late);
     subjectTeaching = subject;
     this.school = school;
+   
   }
+  public int compareToTeacher (Teacher other){
+    if (this.id > other.id){
+      return 1;
+    }
+    else if (other.id > this.id){
+      return -1;
+    }
+    return 0;
+  }
+  
 }
