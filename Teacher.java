19df283a1f29f08f1b41e2f4eac93860980add1b
umernadeem <umernadeem23@gmail.com>
2016-12-25 19:28:17 -0500

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,11 +1,14 @@
 class Teacher extends Customer{
   String subjectTeaching;
   String school;
+  double spendingAllowance;
   
-  public Teacher(String name, int customerNum, int curItems, int late, String subject, String school){
+  public Teacher(String name, int customerNum, int curItems, int late, String subject, String school,
+                double spendingAllowed){
     super (name, customerNum, curItems, late);
     subjectTeaching = subject;
     this.school = school;
+    spendingAllowance = spendingAllowed;
    
   }
   public int compareToTeacher (Teacher other){
@@ -16,6 +19,11 @@ class Teacher extends Customer{
       return -1;
     }
     return 0;
+  }
+  
+  public String toString(){
+    String s = "Subject: " + subject + "School: " + school + "Spending allowance: " + spendingAllowance;
+    return s;
   }
   
 }
