570422319152dc813eb17ec4455da9ae7fd5d122
umernadeem <umernadeem23@gmail.com>
2016-12-27 15:16:53 -0500

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,14 @@
+class Employee{
+  String name;
+  double hourlyRate;
+  int booksShelved;
+  int id;
+  
+  public Employee(String name, double hourlyRate, int booksShelved, int id){
+    this.name = name;
+    this.hourlyRate = hourlyRate;
+    this.booksShelved = booksShelved;
+    this.id = id;
+  }
+  
+}
