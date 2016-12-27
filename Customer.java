969c1762677731246a526e0288a44f0b95094b39
umernadeem <umernadeem23@gmail.com>
2016-12-27 15:51:07 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,29 +2,23 @@ import java.util.Scanner;
 import java.io.*;
 
 // Abstract class customer
-abstract class Customer{
+abstract class Person{
   protected String name; // customer name
-  protected int id; // customer id
-  protected int curAmountOfItems; // current amount of items checked out
-  protected int itemsLate; // amount of items overdue
+  protected int id; // customer i
   
   // Constructor
-  public Customer(String givenName, givenNumber, givenItems, givenItemsLate){
+  public Person(String givenName, givenNumber){
     name = givenName;
     id = givenNumber;
-    curAmountOfItems = givenItems;
-    itemsLate = givenItemsLate;
   }
   
   // Returns Customer info
   public String toString(){
-    String s = "Name: " + name + "\nCustomer Number: " + id + 
-      "\nAmount of items checked out " + curAmountOfItems + "\nItems Late " +
-      itemsLate;
+    String s = "Name: " + name + "\nNumber: " + id;
     return s;
   }
   
-  public int compareToCustomerName (Customer other){
+  public int compareToPersonName (Person other){
     if (name.compareTo(other.name) > 0)
       return 1;
     else if (name.compareTo(other.name) < 0)
@@ -32,30 +26,11 @@ abstract class Customer{
     else
       return 0;
   }
-  public int compareToId (Customer other){
+  public int compareToId (Person other){
     if (this.id > other.id){
       return 1;
     }
     else if (other.id > this.id){
-      return -1;
-    }
-    return 0;
-  }
-  public int compareToItemsLate (Customer other){
-    if (this.itemsLate > other.itemsLate){
-      return 1;
-    }
-    else if (other.itemsLate > this.itemsLate){
-      return -1;
-    }
-    return 0;
-  }
-  
-  public int compareToCurItems (Customer other){
-    if (this.curAmountOfItmes > other.curAmountOfItems){
-      return 1;
-    }
-    else if (other.curAmountOfItems > this.curAmountOfItems){
       return -1;
     }
     return 0;
