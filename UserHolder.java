d9b468544f145747839a0111b83a9f0b21bb89ba
Enderoy <ryan.kwandary@gmail.com>
2016-12-30 21:56:46 -0500

    Add files via upload

---
+++ UserHolder.java
@@ -0,0 +1,24 @@
+abstract class UserHolder {
+	private long id;
+
+	public UserHolder (long id) {
+		this.id = id;
+	}
+
+	abstract boolean isOccupied ();
+
+	abstract String toString ();
+
+	public boolean equals (UserHolder temp) {
+		if (this.id == temp.getId()) {
+			return true;
+		}
+		return false;
+	}
+
+	public long getId () {
+		return id;
+	}
+
+	
+}
