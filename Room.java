49e267fec0d7c15c9563b0e724d8487afce61ac7
Enderoy <ryan.kwandary@gmail.com>
2016-12-30 22:51:38 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,3 +1,18 @@
 public class Room extends UserHolder {
+	private int maxUser;
 
+	public Room (long id, int maxUser) {
+		super(id);
+		this.maxUser = maxUser;
+	}
+
+	public String toString () {
+		return "Max Users: "+maxUser;
+	}
+
+	public boolean isOccupied () {
+
+	}
+
+	
 }
