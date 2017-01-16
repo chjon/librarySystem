6dc29630f650aef64460b0241e2208570ec4819a
raymondgou <raymond_gou@yahoo.ca>
2017-01-16 14:22:43 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,23 +1,23 @@
 public class Room extends UserHolder {
 	private int maxUser;
 
-	public Room (long id, int maxUser) {
-		super(id);
-		this.maxUser = maxUser;
-	}
+ public Room (long id, int maxUser) {
+  	super(id);
+  	this.maxUser = maxUser;
+ }//Room constructor
 
-	public String toString () {
-		return "Max Users: "+maxUser;
-	}
+ public String toString () {
+  	return "Max Users: "+maxUser;
+ }//toString method
 
-	public boolean isOccupied () {
-		if (users != null) {
-			return true;
-		}
-		return false;
-	}
+ public boolean isOccupied () {
+  	if (users != null) {
+   		return true;
+  	}
+  	return false;
+ }//isOccupied method
 
-	public boolean remUser (long) {
-		
-	}
-}
+ public boolean remUser (long) {
+   
+ }//remUser method
+}//Room class
