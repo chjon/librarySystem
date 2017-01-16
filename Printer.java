ad17b65f9e6ed1a53f91a2498a2abf76311183ab
Hiddentester <jonathanchung115@hotmail.com>
2017-01-16 15:07:52 -0500

    Updated comments and fixed tabbing

--- Printer.java
+++ Printer.java
@@ -1,51 +1,64 @@
-public class Printer{
-  protected final double PRICE_BW = 0.15:                   //price of black&white paper per sheet
-  protected final double PRICE_COLOUR = 0.50;               //price of color paper per sheet
-  protected long id;                                        //id of printer
-  protected int maxPaper;                                   //max amount of paper(s) allowed in the printer
-  protected int numPaper;                                   //amount of paper currently in the printer
+/*******************************************************************************
+ * File Name:     Printer.java
+ * Class:         ICS4U-01
+ * Date:          2017/01/16
+ * Description:   This class defines a printer.
+ *******************************************************************************/
+
+public class Printer {
+	protected final double PRICE_BW = 0.15;         //Price of black & white paper per sheet
+	protected final double PRICE_COLOUR = 0.50;     //Price colour paper per sheet
+	protected long id;                              //ID of printer
+	protected int maxPaper;                         //max amount of paper(s) allowed in the printer
+	protected int numPaper;                         //amount of paper currently in the printer
   
-  public Printer(long identification, int max, int num){
-    id = identification;
-    maxPaper = max;
-    numPaper = num;
-  }//Printer constructor
+	public Printer (long identification, int max, int num) {
+		id = identification;
+		maxPaper = max;
+		numPaper = num;
+	} //Printer constructor
   
-  public String toString(){
-    return "ID: "+id + " Max Number of Paper(s): " +  maxPaper + "Current Number of Paper(s): " +numPaper;
-  }//toString method
+	public String toString () {
+		return "ID: " + id + "\n" +
+			"Max Number of Paper(s): " + maxPaper + "\n" +
+			"Current Number of Paper(s): " + numPaper;
+	} //toString method
   
-  public boolean equals(Printer other){
-    if(id == other.id){
-      return true;
-    }
-    return false;
-  }//equals method
+	public boolean equals (Printer other) {
+		if (id == other.id) {
+			return true;
+		} //if structure
+		
+		return false;
+	} //equals method
   
-  public long getId(){
-    return id;
-  }//getID method
+	public long getID () {
+		return id;
+	} //getID method
   
-  public int getNumPaper(){
-    return numPaper;
-  }//getPaper method
+	public int getNumPaper () {
+		return numPaper;
+	} //getPaper method
   
-  public boolean print(boolean color,int needed){  //Checks if printing is possible and adds to amount due
-    if(numPaper - needed >= 0){
-      return true;
-    }
-    return false;
-  }//print method
-  
-  public boolean addPaper(int sheets){  //Adds sheets of paper to the printer
-    if(maxPaper >= sheets + numPaper){
-      numPaper = numPaper + sheets;
-      return true;
-    }
-    else{
-      numPaper = maxPaper;
-      return false;
-    }
-  }//addPaper method
-  
-}//Printer class
+	//Checks if printing is possible and adds to amount due
+	public boolean print (boolean color,int needed) {
+		if(numPaper - needed >= 0){
+			numPaper -= needed;
+			return true;
+		} //if structure
+		
+		numPaper = 0;
+		return false;
+	} //print method
+
+	//Adds sheets of paper to the printer
+	public boolean addPaper (int sheets) {
+		if (maxPaper >= sheets + numPaper) {
+			numPaper = numPaper + sheets;
+			return true;
+		} //if structure
+		
+		numPaper = maxPaper;
+		return false;
+	} //addPaper method 
+} //Printer class
